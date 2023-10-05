ecab2e2dab43570e14f8704d362b302b34e83319
gusgushz <110563903+gusgushz@users.noreply.github.com>
2023-10-05 12:27:33 -0600

    Add files via upload

---
+++ Parcial1/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace Parcial1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAgregar = new Button();
+            btnListado = new Button();
+            btnBuscador = new Button();
+            btnEliminar = new Button();
+            listBox1 = new ListBox();
+            textBoxNombre = new TextBox();
+            textBoxEdad = new TextBox();
+            textBoxGenero = new TextBox();
+            textBoxTelefono = new TextBox();
+            textBoxDiagnostico = new TextBox();
+            labelNombre = new Label();
+            labelEdad = new Label();
+            labelGenero = new Label();
+            labelTelefono = new Label();
+            labelDiagnostico = new Label();
+            buttonEnviarAgr = new Button();
+            buttonCerrarList = new Button();
+            buttonEstad = new Button();
+            buttonEnviarElim = new Button();
+            textBoxID = new TextBox();
+            buttonSalir = new Button();
+            buttonBuscarN = new Button();
+            buttonBuscarD = new Button();
+            SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Location = new Point(82, 48);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(94, 50);
+            btnAgregar.TabIndex = 0;
+            btnAgregar.Text = "Agregar Pacientes";
+            btnAgregar.UseVisualStyleBackColor = true;
+            btnAgregar.Click += btnAgregar_Click;
+            // 
+            // btnListado
+            // 
+            btnListado.Location = new Point(280, 44);
+            btnListado.Name = "btnListado";
+            btnListado.Size = new Size(94, 54);
+            btnListado.TabIndex = 1;
+            btnListado.Text = "Listado de Pacientes";
+            btnListado.UseVisualStyleBackColor = true;
+            btnListado.Click += btnListado_Click;
+            // 
+            // btnBuscador
+            // 
+            btnBuscador.Location = new Point(453, 48);
+            btnBuscador.Name = "btnBuscador";
+            btnBuscador.Size = new Size(94, 55);
+            btnBuscador.TabIndex = 2;
+            btnBuscador.Text = "Buscar Paciente";
+            btnBuscador.UseVisualStyleBackColor = true;
+            btnBuscador.Click += btnBuscador_Click;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Location = new Point(627, 45);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(94, 53);
+            btnEliminar.TabIndex = 3;
+            btnEliminar.Text = "Eliminar Paciente";
+            btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminar.Click += btnEliminar_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.HorizontalScrollbar = true;
+            listBox1.ItemHeight = 20;
+            listBox1.Location = new Point(78, 134);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(643, 164);
+            listBox1.TabIndex = 4;
+            listBox1.Visible = false;
+            // 
+            // textBoxNombre
+            // 
+            textBoxNombre.Location = new Point(218, 141);
+            textBoxNombre.Name = "textBoxNombre";
+            textBoxNombre.Size = new Size(410, 27);
+            textBoxNombre.TabIndex = 5;
+            textBoxNombre.Visible = false;
+            // 
+            // textBoxEdad
+            // 
+            textBoxEdad.Location = new Point(218, 190);
+            textBoxEdad.Name = "textBoxEdad";
+            textBoxEdad.Size = new Size(410, 27);
+            textBoxEdad.TabIndex = 6;
+            textBoxEdad.Visible = false;
+            // 
+            // textBoxGenero
+            // 
+            textBoxGenero.Location = new Point(219, 346);
+            textBoxGenero.Name = "textBoxGenero";
+            textBoxGenero.Size = new Size(410, 27);
+            textBoxGenero.TabIndex = 9;
+            textBoxGenero.Visible = false;
+            // 
+            // textBoxTelefono
+            // 
+            textBoxTelefono.Location = new Point(219, 295);
+            textBoxTelefono.Name = "textBoxTelefono";
+            textBoxTelefono.Size = new Size(410, 27);
+            textBoxTelefono.TabIndex = 8;
+            textBoxTelefono.Visible = false;
+            // 
+            // textBoxDiagnostico
+            // 
+            textBoxDiagnostico.Location = new Point(215, 241);
+            textBoxDiagnostico.Name = "textBoxDiagnostico";
+            textBoxDiagnostico.Size = new Size(410, 27);
+            textBoxDiagnostico.TabIndex = 7;
+            textBoxDiagnostico.Visible = false;
+            // 
+            // labelNombre
+            // 
+            labelNombre.AutoSize = true;
+            labelNombre.Location = new Point(215, 111);
+            labelNombre.Name = "labelNombre";
+            labelNombre.Size = new Size(64, 20);
+            labelNombre.TabIndex = 10;
+            labelNombre.Text = "Nombre";
+            labelNombre.Visible = false;
+            // 
+            // labelEdad
+            // 
+            labelEdad.AutoSize = true;
+            labelEdad.Location = new Point(218, 172);
+            labelEdad.Name = "labelEdad";
+            labelEdad.Size = new Size(43, 20);
+            labelEdad.TabIndex = 11;
+            labelEdad.Text = "Edad";
+            labelEdad.Visible = false;
+            // 
+            // labelGenero
+            // 
+            labelGenero.AutoSize = true;
+            labelGenero.Location = new Point(221, 325);
+            labelGenero.Name = "labelGenero";
+            labelGenero.Size = new Size(57, 20);
+            labelGenero.TabIndex = 12;
+            labelGenero.Text = "Género";
+            labelGenero.Visible = false;
+            // 
+            // labelTelefono
+            // 
+            labelTelefono.AutoSize = true;
+            labelTelefono.Location = new Point(215, 271);
+            labelTelefono.Name = "labelTelefono";
+            labelTelefono.Size = new Size(67, 20);
+            labelTelefono.TabIndex = 13;
+            labelTelefono.Text = "Teléfono";
+            labelTelefono.Visible = false;
+            // 
+            // labelDiagnostico
+            // 
+            labelDiagnostico.AutoSize = true;
+            labelDiagnostico.Location = new Point(216, 218);
+            labelDiagnostico.Name = "labelDiagnostico";
+            labelDiagnostico.Size = new Size(89, 20);
+            labelDiagnostico.TabIndex = 14;
+            labelDiagnostico.Text = "Diagnóstico";
+            labelDiagnostico.Visible = false;
+            // 
+            // buttonEnviarAgr
+            // 
+            buttonEnviarAgr.Location = new Point(289, 393);
+            buttonEnviarAgr.Name = "buttonEnviarAgr";
+            buttonEnviarAgr.Size = new Size(258, 36);
+            buttonEnviarAgr.TabIndex = 15;
+            buttonEnviarAgr.Text = "Agregar";
+            buttonEnviarAgr.UseVisualStyleBackColor = true;
+            buttonEnviarAgr.Visible = false;
+            buttonEnviarAgr.Click += buttonEnviarAgr_Click;
+            // 
+            // buttonCerrarList
+            // 
+            buttonCerrarList.Location = new Point(280, 68);
+            buttonCerrarList.Name = "buttonCerrarList";
+            buttonCerrarList.Size = new Size(267, 45);
+            buttonCerrarList.TabIndex = 16;
+            buttonCerrarList.Text = "Cerrar Listado";
+            buttonCerrarList.UseVisualStyleBackColor = true;
+            buttonCerrarList.Visible = false;
+            buttonCerrarList.Click += buttonCerrarList_Click;
+            // 
+            // buttonEstad
+            // 
+            buttonEstad.Location = new Point(347, 193);
+            buttonEstad.Name = "buttonEstad";
+            buttonEstad.Size = new Size(147, 42);
+            buttonEstad.TabIndex = 17;
+            buttonEstad.Text = "Estadísticas";
+            buttonEstad.UseVisualStyleBackColor = true;
+            buttonEstad.Click += buttonEstad_Click;
+            // 
+            // buttonEnviarElim
+            // 
+            buttonEnviarElim.Location = new Point(284, 379);
+            buttonEnviarElim.Name = "buttonEnviarElim";
+            buttonEnviarElim.Size = new Size(265, 34);
+            buttonEnviarElim.TabIndex = 18;
+            buttonEnviarElim.Text = "Eliminar";
+            buttonEnviarElim.UseVisualStyleBackColor = true;
+            buttonEnviarElim.Visible = false;
+            buttonEnviarElim.Click += buttonEnviarElim_Click;
+            // 
+            // textBoxID
+            // 
+            textBoxID.Location = new Point(219, 190);
+            textBoxID.Name = "textBoxID";
+            textBoxID.Size = new Size(408, 27);
+            textBoxID.TabIndex = 20;
+            textBoxID.Visible = false;
+            // 
+            // buttonSalir
+            // 
+            buttonSalir.Location = new Point(679, 400);
+            buttonSalir.Name = "buttonSalir";
+            buttonSalir.Size = new Size(94, 29);
+            buttonSalir.TabIndex = 21;
+            buttonSalir.Text = "Salir";
+            buttonSalir.UseVisualStyleBackColor = true;
+            buttonSalir.Click += buttonSalir_Click;
+            // 
+            // buttonBuscarN
+            // 
+            buttonBuscarN.Location = new Point(311, 174);
+            buttonBuscarN.Name = "buttonBuscarN";
+            buttonBuscarN.Size = new Size(204, 46);
+            buttonBuscarN.TabIndex = 22;
+            buttonBuscarN.Text = "Buscar con Nombre";
+            buttonBuscarN.UseVisualStyleBackColor = true;
+            buttonBuscarN.Visible = false;
+            buttonBuscarN.Click += buttonBuscarN_Click;
+            // 
+            // buttonBuscarD
+            // 
+            buttonBuscarD.Location = new Point(311, 273);
+            buttonBuscarD.Name = "buttonBuscarD";
+            buttonBuscarD.Size = new Size(204, 49);
+            buttonBuscarD.TabIndex = 23;
+            buttonBuscarD.Text = "Buscar con Diagnóstico";
+            buttonBuscarD.UseVisualStyleBackColor = true;
+            buttonBuscarD.Visible = false;
+            buttonBuscarD.Click += buttonBuscarD_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonBuscarD);
+            Controls.Add(buttonBuscarN);
+            Controls.Add(buttonSalir);
+            Controls.Add(textBoxID);
+            Controls.Add(buttonEnviarElim);
+            Controls.Add(buttonEstad);
+            Controls.Add(buttonCerrarList);
+            Controls.Add(buttonEnviarAgr);
+            Controls.Add(labelDiagnostico);
+            Controls.Add(labelTelefono);
+            Controls.Add(labelGenero);
+            Controls.Add(labelEdad);
+            Controls.Add(labelNombre);
+            Controls.Add(textBoxDiagnostico);
+            Controls.Add(textBoxTelefono);
+            Controls.Add(textBoxGenero);
+            Controls.Add(textBoxEdad);
+            Controls.Add(textBoxNombre);
+            Controls.Add(listBox1);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnBuscador);
+            Controls.Add(btnListado);
+            Controls.Add(btnAgregar);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnAgregar;
+        private Button btnListado;
+        private Button btnBuscador;
+        private Button btnEliminar;
+        private ListBox listBox1;
+        private TextBox textBoxNombre;
+        private TextBox textBoxEdad;
+        private TextBox textBoxGenero;
+        private TextBox textBoxTelefono;
+        private TextBox textBoxDiagnostico;
+        private Label labelNombre;
+        private Label labelEdad;
+        private Label labelGenero;
+        private Label labelTelefono;
+        private Label labelDiagnostico;
+        private Button buttonEnviarAgr;
+        private Button buttonCerrarList;
+        private Button buttonEstad;
+        private Button buttonEnviarElim;
+        private TextBox textBoxID;
+        private Button buttonSalir;
+        private Button buttonBuscarN;
+        private Button buttonBuscarD;
+    }
+}
---
+++ Parcial1/Form1.cs
@@ -0,0 +1,346 @@
+using System.Collections.Generic;
+using System.Windows.Forms;
+using static Parcial1.Form1;
+
+namespace Parcial1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Paciente> Pacientes = new List<Paciente>();
+        RegistroPacientes FaroDelMayab = new RegistroPacientes();
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            btnAgregar.Visible = false;
+            labelNombre.Visible = true;
+            labelEdad.Visible = true;
+            labelGenero.Visible = true;
+            labelTelefono.Visible = true;
+            labelDiagnostico.Visible = true;
+            textBoxNombre.Visible = true;
+            textBoxEdad.Visible = true;
+            textBoxGenero.Visible = true;
+            textBoxTelefono.Visible = true;
+            textBoxDiagnostico.Visible = true;
+            buttonEnviarAgr.Visible = true;
+
+            btnListado.Visible = false;
+            btnBuscador.Visible = false;
+            btnEliminar.Visible = false;
+            buttonEnviarElim.Visible = false;
+            buttonEstad.Visible = false;
+
+            buttonSalir.Visible = true;
+        }
+        private void buttonEnviarAgr_Click(object sender, EventArgs e)
+        {
+            string nombre = textBoxNombre.Text;
+            int edad = int.Parse(textBoxEdad.Text);
+            string genero = textBoxGenero.Text;
+            string telefono = textBoxTelefono.Text;
+            string diagnostico = textBoxDiagnostico.Text;
+            FaroDelMayab.AgregarPacientes(Pacientes, nombre, edad, genero, telefono, diagnostico);
+            textBoxNombre.Clear();
+            textBoxEdad.Clear();
+            textBoxGenero.Clear();
+            textBoxTelefono.Clear();
+            textBoxDiagnostico.Clear();
+            btnAgregar.Visible = true;
+            labelNombre.Visible = false;
+            labelEdad.Visible = false;
+            labelGenero.Visible = false;
+            labelTelefono.Visible = false;
+            labelDiagnostico.Visible = false;
+            textBoxNombre.Visible = false;
+            textBoxEdad.Visible = false;
+            textBoxGenero.Visible = false;
+            textBoxTelefono.Visible = false;
+            textBoxDiagnostico.Visible = false;
+            buttonEnviarAgr.Visible = false;
+
+            btnListado.Visible = true;
+            btnBuscador.Visible = true;
+            btnEliminar.Visible = true;
+            buttonEnviarElim.Visible = false;
+            buttonEstad.Visible = true;
+
+            buttonSalir.Visible = false;
+        }
+        private void btnListado_Click(object sender, EventArgs e)
+        {
+            btnListado.Visible = false;
+            listBox1.Items.Clear();
+            listBox1.Visible = true;
+
+            FaroDelMayab.MostrarPacientes(Pacientes, listBox1);
+            buttonCerrarList.Visible = true;
+
+            btnAgregar.Visible = false;
+            btnBuscador.Visible = false;
+            btnEliminar.Visible = false;
+            buttonEnviarElim.Visible = false;
+            buttonEstad.Visible = false;
+        }
+        private void buttonCerrarList_Click(object sender, EventArgs e)
+        {
+            btnListado.Visible = true;
+            listBox1.Items.Clear();
+            listBox1.Visible = false;
+            buttonCerrarList.Visible = false;
+
+            btnAgregar.Visible = true;
+            btnBuscador.Visible = true;
+            btnEliminar.Visible = true;
+            buttonEnviarElim.Visible = false;
+            buttonEstad.Visible = true;
+        }
+
+        private void btnBuscador_Click(object sender, EventArgs e)
+        {
+            btnAgregar.Visible = false;
+            btnListado.Visible = false;
+            btnBuscador.Visible = false;
+            btnEliminar.Visible = false;
+            buttonEnviarElim.Visible = false;
+            buttonEstad.Visible = false;
+
+            buttonBuscarN.Visible = true;
+            buttonBuscarD.Visible = true;
+
+            labelNombre.Visible = true;
+            labelDiagnostico.Visible = true;
+            textBoxNombre.Visible = true;
+            textBoxDiagnostico.Visible = true;
+
+            buttonSalir.Visible = true;
+        }
+
+        private void buttonBuscarN_Click(object sender, EventArgs e)
+        {
+            string nombre = textBoxNombre.Text;
+            listBox1.Visible = true;
+            List<Paciente> pacientesEncontrados = FaroDelMayab.BuscarPacientesNombre(Pacientes, nombre);
+            foreach (Paciente paciente in pacientesEncontrados)
+            {
+                listBox1.Items.Add($"ID:{paciente.Id} | Nombre: {paciente.Nombre} | Edad: {paciente.Edad} | Género: {paciente.Genero} | Teléfono: {paciente.Telefono} | Diagnóstico: {paciente.Diagnostico}");
+            }
+
+            buttonBuscarN.Visible = false;
+            buttonBuscarD.Visible = false;
+
+            labelNombre.Visible = false;
+            labelDiagnostico.Visible = false;
+            textBoxNombre.Clear();
+            textBoxNombre.Visible = false;
+            textBoxDiagnostico.Clear();
+            textBoxDiagnostico.Visible = false;
+
+            buttonCerrarList.Visible = true;
+
+            btnListado.Visible = false;
+            btnBuscador.Visible = false;
+            btnEliminar.Visible = false;
+            buttonEnviarElim.Visible = false;
+            buttonEstad.Visible = false;
+
+            buttonSalir.Visible = true;
+        }
+        private void buttonBuscarD_Click(object sender, EventArgs e)
+        {
+            string diagnostico = textBoxDiagnostico.Text;
+            listBox1.Visible = true;
+            List<Paciente> pacientesEncontrados = FaroDelMayab.BuscarPacientesDiagnostico(Pacientes, diagnostico);
+            foreach (Paciente paciente in pacientesEncontrados)
+            {
+                listBox1.Items.Add($"ID:{paciente.Id} | Nombre: {paciente.Nombre} | Edad: {paciente.Edad} | Género: {paciente.Genero} | Teléfono: {paciente.Telefono} | Diagnóstico: {paciente.Diagnostico}");
+            }
+
+            buttonBuscarN.Visible = false;
+            buttonBuscarD.Visible = false;
+
+            labelNombre.Visible = false;
+            labelDiagnostico.Visible = false;
+            textBoxNombre.Clear();
+            textBoxNombre.Visible = false;
+            textBoxDiagnostico.Clear();
+            textBoxDiagnostico.Visible = false;
+
+            buttonCerrarList.Visible = true;
+
+            btnListado.Visible = false;
+            btnBuscador.Visible = false;
+            btnEliminar.Visible = false;
+            buttonEnviarElim.Visible = false;
+            buttonEstad.Visible = false;
+
+            buttonSalir.Visible = true;
+        }
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            labelEdad.Visible = true;
+            labelEdad.Text = "ID";
+            textBoxID.Visible = true;
+            btnAgregar.Visible = false;
+            btnBuscador.Visible = false;
+            btnListado.Visible = false;
+            btnEliminar.Visible = false;
+            buttonEnviarElim.Visible = true;
+            buttonEstad.Visible = false;
+
+            buttonSalir.Visible = true;
+        }
+        private void buttonEnviarElim_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(textBoxID.Text);
+            FaroDelMayab.EliminarPaciente(Pacientes, id);
+
+            labelEdad.Visible = false;
+            labelEdad.Text = "Edad";
+            textBoxID.Visible = false;
+
+            textBoxID.Clear();
+
+            btnAgregar.Visible = true;
+            btnBuscador.Visible = true;
+            btnListado.Visible = true;
+            btnEliminar.Visible = true;
+            buttonEnviarElim.Visible = false;
+            buttonEstad.Visible = true;
+
+            buttonSalir.Visible = false;
+        }
+        private void buttonEstad_Click(object sender, EventArgs e)
+        {
+            btnAgregar.Visible = false;
+            btnListado.Visible = false;
+            btnBuscador.Visible = false;
+            btnEliminar.Visible = false;
+            buttonEstad.Visible = false;
+            listBox1.Visible = true;
+            buttonSalir.Visible = true;
+            double[] Estadisticas = FaroDelMayab.CalcularEstadisticasPacientes(Pacientes);
+            listBox1.Items.Add($"Total de pacientes: {Estadisticas[0]} | Promedio de edad de pacientes: {Estadisticas[1]}");
+        }
+        private void buttonSalir_Click(object sender, EventArgs e)
+        {
+            btnAgregar.Visible = true;
+            btnListado.Visible = true;
+            btnBuscador.Visible = true;
+            btnEliminar.Visible = true;
+            buttonEstad.Visible = true;
+            listBox1.Visible = false;
+            listBox1.Items.Clear();
+            textBoxNombre.Visible = false;
+            textBoxNombre.Clear();
+            textBoxEdad.Visible = false;
+            textBoxEdad.Clear();
+            textBoxGenero.Visible = false;
+            textBoxGenero.Clear();
+            textBoxTelefono.Visible = false;
+            textBoxTelefono.Clear();
+            textBoxDiagnostico.Visible = false;
+            textBoxDiagnostico.Clear();
+            labelNombre.Visible = false;
+            labelEdad.Visible = false;
+            labelEdad.Text = "Edad";
+            labelGenero.Visible = false;
+            labelTelefono.Visible = false;
+            labelDiagnostico.Visible = false;
+            buttonEnviarAgr.Visible = false;
+            buttonCerrarList.Visible = false;
+            buttonEnviarElim.Visible = false;
+            textBoxID.Visible = false;
+            textBoxID.Clear();
+            buttonSalir.Visible = false;
+            buttonBuscarN.Visible = false;
+            buttonBuscarD.Visible = false;
+        }
+        public class Paciente
+        {
+            private static int proximoId = 1;
+            public int Id { get; private set; } = 0;
+            public string Nombre { get; set; } = null!;
+            public int Edad { get; set; }
+            public string Genero { get; set; } = null!;
+            public string Telefono { get; set; } = null!;
+            public string Diagnostico { get; set; } = null!;
+
+            public Paciente()
+            {
+                Id = proximoId;
+                proximoId++;
+            }
+        }
+        public class RegistroPacientes
+        {
+            public void AgregarPacientes(List<Paciente> Pacientes, string nombre, int edad, string genero, string telefono, string diagnostico)
+            {
+                Pacientes.Add(new Paciente { Nombre = nombre, Edad = edad, Genero = genero, Telefono = telefono, Diagnostico = diagnostico });
+            }
+
+            public List<Paciente> BuscarPacientesNombre(List<Paciente> Pacientes, string nombre)
+            {
+                List<Paciente> pacientesEncontrados = new List<Paciente>();
+                foreach (Paciente paciente in Pacientes)
+                {
+                    if (paciente.Nombre.ToLower().Contains(nombre.ToLower()))
+                    {
+                        pacientesEncontrados.Add(paciente);
+                    }
+                }
+                return pacientesEncontrados;
+            }
+
+            public List<Paciente> BuscarPacientesDiagnostico(List<Paciente> Pacientes, string diagnostico)
+            {
+                List<Paciente> pacientesEncontrados = new List<Paciente>();
+                foreach (Paciente paciente in Pacientes)
+                {
+                    if (paciente.Diagnostico.ToLower().Contains(diagnostico.ToLower()))
+                    {
+                        pacientesEncontrados.Add(paciente);
+                    }
+                }
+                return pacientesEncontrados;
+            }
+
+            public void MostrarPacientes(List<Paciente> Pacientes, ListBox listBox1)
+            {
+                foreach (Paciente paciente in Pacientes)
+                {
+                    listBox1.Items.Add($"ID:{paciente.Id} | Nombre: {paciente.Nombre} | Edad: {paciente.Edad} | Género: {paciente.Genero} | Teléfono: {paciente.Telefono} | Diagnóstico: {paciente.Diagnostico}");
+                }
+            }
+
+            public void EliminarPaciente(List<Paciente> Pacientes, int id)
+            {
+                Paciente pacienteEntrado = Pacientes.Find(p => p.Id == id)!;
+                if (pacienteEntrado == null)
+                {
+                    MessageBox.Show("No se encontro un paciente con ese ID");
+                    return;
+                }
+                else
+                {
+                    Pacientes.Remove(pacienteEntrado);
+                }
+            }
+            public double[] CalcularEstadisticasPacientes(List<Paciente> Pacientes)
+            {
+                double totalPacientes = Pacientes.Count();
+                double edadPromedioPacientes = 0;
+                foreach (Paciente paciente in Pacientes)
+                {
+                    edadPromedioPacientes += paciente.Edad;
+                }
+                edadPromedioPacientes /= totalPacientes;
+
+                double[] Resultados = { totalPacientes, edadPromedioPacientes };
+                return Resultados;
+            }
+        }
+    }
+}
